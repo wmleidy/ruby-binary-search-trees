e36bf146478a819d586e1312bd69a76c08f0c3ad
Bill Leidy <wmleidy@gmail.com>
2015-11-28 23:17:32 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# ruby-binary-search-trees
-An unpolished version of a Binary Search Trees (BST) in Ruby
+# Binary Search Trees (BST) in Ruby
+
+This repo contains one possible implementation of a binary search tree in Ruby. I am posting it because a routine Google search revealed there are surprisingly few sources have a good (legible and working) Ruby implementation of the core methods of a BST. So, for those who are taking a CS class on data structures or need a refresher to prepare for the whiteboarding part of a technical interview, this repo might be of interest to you.
+
+There is no one right way to skin a cat, and neither is there one right way to write a BST class. My methods are all recursive, but iterative methods are possible too in some cases. And there are assuredly efficiency gains to be made too within the recursive algorithms (e.g. all the checks for .nil? could probably be refactored)--this is all fairly unpolished. But I think it's helpful to see others' approaches even if they might be suboptimal (I know I've benefited from the Internet's collective Ruby wisdom countless times), so I hope this provides some food for thought on one way to approach BSTs in Ruby.
+
+Methods present:
+```
+#add_value
+#has_value?
+#delete_value  (destructive except at a root with fewer than two children...needs to be fixed)
+
+#size
+#height
+#valid_bst?
+#identical_trees?
+#height_balanced?
+
+#inorder   (traversal, with conversions to both a sorted array and an ascending linked list)
+#preorder  (traversal)
+#postorder (traversal)
+
+#leaf?
+#total_leaves
+```
+
+What's missing? Well, a `#delete` method that works properly at a non-full root (I welcome any suggestions), and a `#rotation` method that would self-balance the tree when new items are added that cause the tree to become unbalanced. (There are many flavors of these: [click here](https://en.wikipedia.org/wiki/Self-balancing_binary_search_tree).)
---
+++ binary_search_trees.rb
@@ -0,0 +1,224 @@
+class Tree
+	attr_accessor :value, :left, :right
+
+	def initialize(value)
+		@value = value
+		@left = nil
+		@right = nil
+	end
+
+	def add_value(value, root = self)
+		if root.nil?
+			return root = Tree.new(value)
+		elsif value > root.value
+			root.right = add_value(value, root.right)
+			return root
+		elsif value < root.value
+			root.left = add_value(value, root.left)
+			return root
+		else
+			raise ArgumentError.new "Value already exists in tree."
+		end
+	end
+
+	def has_value?(value, root = self)
+		if root.nil?
+			return false
+		elsif value == root.value
+			return true
+		elsif value > root.value
+			has_value?(value, root.right)
+		elsif value < root.value
+			has_value?(value, root.left)
+		end
+	end
+
+	def size(root = self)
+		return 0 if root.nil?
+		1 + size(root.left) + size(root.right)
+	end
+
+	def leaf?
+		left.nil? && right.nil?
+	end
+
+	def total_leaves(root = self)
+		return 0 if root.nil?
+		return 1 if root.leaf?
+		total_leaves(root.left) + total_leaves(root.right)
+	end
+
+	# def height(root = self, max = 0, counter = 0)
+	# 	if root.leaf?
+	# 		max = counter if counter > max
+	# 	else
+	# 		counter += 1
+	# 		max = height(root.left, max, counter) unless root.left.nil?
+	# 		max = height(root.right, max, counter) unless root.right.nil?
+	# 	end
+	# 	max
+	# end
+
+	# slicker, though harder-to-parse version
+	def height(root = self)
+		return 0 if root.nil? || root.leaf?
+		1 + [height(root.left), height(root.right)].max
+	end
+
+	def height_balanced?(root = self, levels = 0, max = self.height)
+		if root.leaf?
+			levels + 1 >= max
+		else
+			levels += 1
+			if root.left.nil?
+				height_balanced?(root.right, levels, max)
+			elsif root.right.nil?
+				height_balanced?(root.left, levels, max)
+			else
+				height_balanced?(root.left, levels, max) && height_balanced?(root.right, levels, max)
+			end
+		end
+	end
+
+	def identical_trees?(tree2, tree1 = self)
+		if tree1.nil? && tree2.nil?
+			return true
+		elsif tree1.nil? || tree2.nil?
+			return false
+		elsif tree1.value != tree2.value
+			return false
+		else
+			identical_trees?(tree1.left, tree2.left) && identical_trees?(tree1.right, tree2.right)
+		end
+	end
+
+	# works properly (destructively) in most situations...
+	# however, when deleting value at root of tree if one (or both) of its branches is nil,
+	# then #delete will return the correct tree, but do so non-destructively
+	# NB. this (I believe) has to do with how Ruby treats self
+	def delete_value(value, root = self)
+		if root.nil?
+			return
+		elsif value > root.value
+			root.right = delete_value(value, root.right)
+		elsif value < root.value
+			root.left = delete_value(value, root.left)
+		else # i.e. value == root.value
+			if root.left.nil?  # also catches cases when both child nodes are empty
+				root = root.right
+			elsif root.right.nil?
+				root = root.left
+			else
+				root = shift_values(root)
+			end
+		end
+		root
+	end
+
+	### three helper methods for #delete ###
+	def shift_values(root)
+		root.value = get_next_value(root.right)
+		root.right = right_subtree_without_next_value(root.right)
+		root
+	end
+
+	def get_next_value(root)
+		if root.left.nil?
+			root.value
+		else
+			get_next_value(root.left)
+		end
+	end
+
+	def right_subtree_without_next_value(root)
+		if root.left.nil?
+			return root.right
+		else
+			root.left = right_subtree_without_next_value(root.left)
+		end
+		root
+	end
+	### end helper methods for #delete ###
+
+	def valid_bst?(root = self, min = -1.0/0.0, max = 1.0/0.0)
+		if root.value > min && root.value < max
+			if root.leaf?
+				return true
+			elsif root.right.nil?
+				valid_bst?(root.left, min, root.value)
+			elsif root.left.nil?
+				valid_bst?(root.right, root.value, max)
+			else
+				valid_bst?(root.left, min, root.value) && valid_bst?(root.right, root.value, max)
+			end
+		else
+			false
+		end
+	end
+
+	# alt method for valid_bst?
+	# convert to linked list, then check whether it's sorted (works in O(n))
+	def alt_valid_bst?
+		self.to_ll.sorted?
+	end
+
+	### both of these conversion methods are in-order traversals ###
+	def to_a (root = self, array = [])
+		to_a(root.left, array) unless root.left.nil?
+		array << root.value
+		to_a(root.right, array) unless root.right.nil?
+		array
+	end
+
+	def to_ll (root = self, list = Node.new)
+		to_ll(root.left, list) unless root.left.nil?
+		list.value.nil? ? list.value = root.value : list << root.value
+		to_ll(root.right, list) unless root.right.nil?
+		list
+	end
+	### (i.e. left node, current node, right node)
+
+	# pre-order traversal (current, left, right)
+	def preorder (root = self, array = [])
+		array << root.value
+		preorder(root.left, array) unless root.left.nil?
+		preorder(root.right, array) unless root.right.nil?
+		array
+	end
+
+	# post-order traversal (left, right, current)
+	def postorder (root = self, array = [])
+		postorder(root.left, array) unless root.left.nil?
+		postorder(root.right, array) unless root.right.nil?
+		array << root.value
+		array
+	end
+
+end
+
+class Node
+	attr_accessor :value, :next
+
+	def initialize(value = nil)
+		@value = value
+		@next = nil
+	end
+
+	def << (value, pointer = self)
+		if pointer.next
+			pointer.next << value
+		else
+			pointer.next = Node.new(value)
+		end
+		self
+	end
+
+	def sorted?(pointer = self)
+		while pointer.next
+			return false unless pointer.value < pointer.next.value
+			pointer = pointer.next
+		end
+		true
+	end
+
+end
